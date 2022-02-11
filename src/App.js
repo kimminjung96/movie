2eed941a478a8c572a38e526d9e1a5bf5d634de3
kimminjung96 <dds13462@gmail.com>
2022-02-11 20:07:41 +0900

    summary.lenght

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail"
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/hello"  element={<h1>hello</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
